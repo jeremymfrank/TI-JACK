9e5eda9e943a93ff4cdef51d84f43bad6fb1845b
Jawatech <frankbeans86@gmail.com>
2026-09-12 11:40:48 -0500

    Bump Android UI patch to v0.14.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tijack.evo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.14"
+        versionCode = 15
+        versionName = "0.14.1"
     }
 
     buildTypes {
