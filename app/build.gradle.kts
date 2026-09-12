573b08f29a00e3f1c9ebc4ecb77c58c43f84e7fa
Jawatech <frankbeans86@gmail.com>
2026-09-12 12:52:54 -0500

    Bump Android polish build to v0.15.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tijack.evo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.15"
+        versionCode = 17
+        versionName = "0.15.1"
     }
 
     buildTypes {
