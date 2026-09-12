4e4d39d0c1b7bf1caa94494419a3c850c1670c7a
Jawatech <frankbeans86@gmail.com>
2026-09-12 08:22:28 -0500

    Bump Android auto-detach test to v0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tijack.evo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.11"
+        versionCode = 12
+        versionName = "0.12"
     }
 
     buildTypes {
