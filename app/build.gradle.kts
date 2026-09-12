9b35646513f785f771e71fa69e9d13ba03a4b364
Jawatech <frankbeans86@gmail.com>
2026-09-12 00:41:14 -0500

    Bump Android transfer test to v0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tijack.evo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
     }
 
     buildTypes {
