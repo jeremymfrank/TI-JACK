047452fa2e3efc62293a1d5defe9a1aed4508e94
Jawatech <frankbeans86@gmail.com>
2026-09-12 20:10:40 -0500

    Bump Android preview to v0.19.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tijack.evo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.19.1"
+        versionCode = 27
+        versionName = "0.19.2"
     }
 
     buildTypes {
