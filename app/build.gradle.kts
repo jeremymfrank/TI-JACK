29c15da308cffd56ad5828db81d17d4d1acf2281
Jawatech <frankbeans86@gmail.com>
2026-09-13 08:28:59 -0500

    Bump Android version to v0.21.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tijack.evo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 29
-        versionName = "0.20.1"
+        versionCode = 30
+        versionName = "0.21.0"
     }
 
     buildTypes {
